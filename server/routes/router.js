9345d67c6c7a103af56b98226dfc27030f271aee
krizer54 <74391971+krizer54@users.noreply.github.com>
2022-12-27 06:52:45 +0000

    add img

--- server/routes/router.js
+++ server/routes/router.js
@@ -1,5 +1,12 @@
+const { application } = require('express');
 const express = require('express');
 const route = express.Router()
 
+route.get("/", (req,res)=> {
+    res.render("index")
+})
 
+route.get("/add-user", (req,res)=> {
+    res.render("add_user")
+})
 module.exports = route
